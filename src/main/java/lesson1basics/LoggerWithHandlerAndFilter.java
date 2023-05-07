32f7398227738a9289f9b5ffa9ed5462af641101
Halevi, Asaf <asaf.halevi@cellwize.com>
2023-05-07 10:22:59 +0300

    [0000] add .editorconfig

--- src/main/java/lesson1basics/LoggerWithHandlerAndFilter.java
+++ src/main/java/lesson1basics/LoggerWithHandlerAndFilter.java
@@ -12,26 +12,26 @@ public class LoggerWithHandlerAndFilter {
 
     public static void main(String[] args) throws IOException {
         //Adding a filter to the logger
-//        Filter filter = new MyFilterImpl();
-//        logger.setFilter(filter);
+        //        Filter filter = new MyFilterImpl();
+        //        logger.setFilter(filter);
 
         //Adding and removing a console handler
-//        Handler consoleHandler = new ConsoleHandler();
-//        logger.addHandler(consoleHandler);
-//        logger.removeHandler(consoleHandler);
+        //        Handler consoleHandler = new ConsoleHandler();
+        //        logger.addHandler(consoleHandler);
+        //        logger.removeHandler(consoleHandler);
 
         //Adding a file handler
-//        Handler fileHandler = new FileHandler(LOG_FILE);
-//        logger.addHandler(fileHandler);
+        //        Handler fileHandler = new FileHandler(LOG_FILE);
+        //        logger.addHandler(fileHandler);
 
         //Setting handler's level
-//        fileHandler.setLevel(Level.WARNING);
+        //        fileHandler.setLevel(Level.WARNING);
         // Adding a filter to the handler
-//        fileHandler.setFilter(filter);
+        //        fileHandler.setFilter(filter);
 
         //Adding a formatter to the handler
-//        MyFormatterImpl formatter = new MyFormatterImpl();
-//        fileHandler.setFormatter(formatter);
+        //        MyFormatterImpl formatter = new MyFormatterImpl();
+        //        fileHandler.setFormatter(formatter);
 
         logger.info("Some information");
         logger.warning("Warning! Warning! Warning!");
